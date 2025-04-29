950535c24025dcce51034032d9d21880cf5f3981
Snayt1k3 <snayt1k3twitch@gmail.com>
2025-04-29 11:00:44 +0300

    add gems

--- gachas-service/scripts/genshin/ascension.sql
+++ gachas-service/scripts/genshin/ascension.sql
@@ -1,289 +1,289 @@
-INSERT INTO ascension_materials (id, local_specialty, boss_drops, created_at, updated_at)
+INSERT INTO ascension_materials (id, local_specialty, boss_drops, gem, created_at, updated_at)
 VALUES 
 -- 1 — Charlotte
-(1, 'Beryl Conch', 'Tour­bil­lon De­vice', NOW(), NOW()),
+(1, 'Beryl Conch', 'Tour­bil­lon De­vice', "Shivada Jade", NOW(), NOW()),
 
 -- 2 — Chevreuse
-(2, 'Lumidouce Bell', 'Fonte­mer Uni­horn', NOW(), NOW()),
+(2, 'Lumidouce Bell', 'Fonte­mer Uni­horn', "Ag­ni­dus A­gate", NOW(), NOW()),
 
 -- 3 — Clorinde
-(3, 'Lumitoile', 'Fonte­mer Uni­horn', NOW(), NOW()),
+(3, 'Lumitoile', 'Fonte­mer Uni­horn', "Vaj­ra­da A­me­thyst", NOW(), NOW()),
 
 -- 4 — Emilie
-(4, 'Lakelight Lily', 'Fragment of a Golden Melody', NOW(), NOW()),
+(4, 'Lakelight Lily', 'Fragment of a Golden Melody', "Na­ga­dus Em­er­ald", NOW(), NOW()),
 
 -- 5 — Freminet
-(5, 'Romaritime Flower', 'Artificed Spare Clockwork Component – Coppelius', NOW(), NOW()),
+(5, 'Romaritime Flower', 'Artificed Spare Clockwork Component – Coppelius', "Shivada Jade", NOW(), NOW()),
 
 -- 6 — Furina
-(6, 'Lakelight Lily', 'Water That Failed to Transcend', NOW(), NOW()),
+(6, 'Lakelight Lily', 'Water That Failed to Transcend', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 7 — Lynette
-(7, 'Lumidouce Bell', 'Ar­ti­ficed Spare Clock­work Com­po­nent — Cop­pe­li­a', NOW(), NOW()),
+(7, 'Lumidouce Bell', 'Ar­ti­ficed Spare Clock­work Com­po­nent — Cop­pe­li­a', "Va­yu­da Tur­quoise", NOW(), NOW()),
 
 -- 8 — Lyney
-(8, 'Rainbow Rose', 'Emperor’s Resolution', NOW(), NOW()),
+(8, 'Rainbow Rose', 'Emperor’s Resolution', "Ag­ni­dus A­gate", NOW(), NOW()),
 
 -- 9 — Navia
-(9, 'Spring of the First Dewdrop', 'Ar­ti­ficed Spare Clock­work Com­po­nent — Cop­pe­li­us', NOW(), NOW()),
+(9, 'Spring of the First Dewdrop', 'Ar­ti­ficed Spare Clock­work Com­po­nent — Cop­pe­li­us', "Prithiva Topaz", NOW(), NOW()),
 
 -- 10 — Neuvillette
-(10, 'Lumitoile', 'Fontemer Unihorn', NOW(), NOW()),
+(10, 'Lumitoile', 'Fontemer Unihorn', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 11 — Sigewinne
-(11, 'Romaritime Flower', 'Water That Failed to Transcend', NOW(), NOW()),
+(11, 'Romaritime Flower', 'Water That Failed to Transcend', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 12 — Wriothesley
-(12, 'Subdetection Unit', 'Tourbillon Device', NOW(), NOW()),
+(12, 'Subdetection Unit', 'Tourbillon Device',"Shivada Jade", NOW(), NOW()),
 
 -- 13 — Arataki Itto
-(13, 'Onikabuto', 'Riftborn Regalia', NOW(), NOW()),
+(13, 'Onikabuto', 'Riftborn Regalia', "Prithiva Topaz",NOW(), NOW()),
 
 -- 14 — Chiori
-(14, 'Dendrobium', 'Ar­ti­ficed Spare Clock­work Com­po­nent — Cop­pe­li­a', NOW(), NOW()),
+(14, 'Dendrobium', 'Ar­ti­ficed Spare Clock­work Com­po­nent — Cop­pe­li­a',"Prithiva Topaz", NOW(), NOW()),
 
 -- 15 — Gorou
-(15, 'Sango Pearl', 'Perpetual Heart', NOW(), NOW()),
+(15, 'Sango Pearl', 'Perpetual Heart',"Prithiva Topaz", NOW(), NOW()),
 
 -- 16 — Kaedehara Kazuha
-(16, 'Sea Ganoderma', 'Marionette Core', NOW(), NOW()),
+(16, 'Sea Ganoderma', 'Marionette Core', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 17 — Kamisato Ayaka
-(17, 'Sakura Bloom', 'Perpetual Heart', NOW(), NOW()),
+(17, 'Sakura Bloom', 'Perpetual Heart', "Shivada Jade", NOW(), NOW()),
 
 -- 18 — Kamisato Ayato
-(18, 'Sakura Bloom', 'Dew of Re­pu­di­a­tion', NOW(), NOW()),
+(18, 'Sakura Bloom', 'Dew of Re­pu­di­a­tion',"Shivada Jade", NOW(), NOW()),
 
 -- 19 — Kirara
-(19, 'Amakumo Fruit', 'Evergloom Ring', NOW(), NOW()),
+(19, 'Amakumo Fruit', 'Evergloom Ring', "Na­ga­dus Em­er­ald", NOW(), NOW()),
 
 -- 20 — Kujou Sara
-(20, 'Dendrobium', 'Storm Beads', NOW(), NOW()),
+(20, 'Dendrobium', 'Storm Beads', "Vaj­ra­da A­me­thyst", NOW(), NOW()),
 
 -- 21 — Kuki Shinobu
-(21, 'Naku Weed', 'Runic Fang', NOW(), NOW()),
+(21, 'Naku Weed', 'Runic Fang', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 22 — Raiden Shogun
-(22, 'Amakumo Fruit', 'Storm Beads', NOW(), NOW()),
+(22, 'Amakumo Fruit', 'Storm Beads', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 23 — Sangonomiya Kokomi
-(23, 'Sango Pearl', 'Dew of Repudiation', NOW(), NOW()),
+(23, 'Sango Pearl', 'Dew of Repudiation', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 24 — Sayu
-(24, 'Crystal Marrow', 'Marionette Core', NOW(), NOW()),
+(24, 'Crystal Marrow', 'Marionette Core', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 25 — Shikanoin Heizou
-(25, 'Onikabuto', 'Runic Fang', NOW(), NOW()),
+(25, 'Onikabuto', 'Runic Fang', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 26 — Thoma
-(26, 'Fluorescent Fungus', 'Smoldering Pearl', NOW(), NOW()),
+(26, 'Fluorescent Fungus', 'Smoldering Pearl', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 27 — Yae Miko
-(27, 'Sea Ganoderma', 'Dragonheir’s False Fin', NOW(), NOW()),
+(27, 'Sea Ganoderma', 'Dragonheir’s False Fin', "Vaj­ra­da A­me­thyst", NOW(), NOW()),
 
 -- 28 — Yoimiya
-(28, 'Naku Weed', 'Smoldering Pearl', NOW(), NOW()),
+(28, 'Naku Weed', 'Smoldering Pearl', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 29 — Yumemizuki Mizuki
-(29, 'Sea Ganoderma', 'Talisman of the Enigmatic Land', NOW(), NOW()),
+(29, 'Sea Ganoderma', 'Talisman of the Enigmatic Land', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 30 — Alhaitham
-(30, 'Sand Grease Pupa', 'Pseudo-Stamens', NOW(), NOW()),
+(30, 'Sand Grease Pupa', 'Pseudo-Stamens',"Na­ga­dus Em­er­ald", NOW(), NOW()),
 
 -- 31 — Candace
-(31, 'Henna Berry', 'Light Guiding Tetrahedron', NOW(), NOW()),
+(31, 'Henna Berry', 'Light Guiding Tetrahedron', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 32 — Collei
-(32, 'Rukkhashava Mushroom', 'Majestic Hooked Beak', NOW(), NOW()),
+(32, 'Rukkhashava Mushroom', 'Majestic Hooked Beak', "Na­ga­dus Em­er­ald",NOW(), NOW()),
 
 -- 33 — Cyno
-(33, 'Scarab', 'Thunderclap Fruitcore', NOW(), NOW()),
+(33, 'Scarab', 'Thunderclap Fruitcore', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 34 — Dehya
-(34, 'Sand Grease Pupa', 'Light Guiding Tetrahedron', NOW(), NOW()),
+(34, 'Sand Grease Pupa', 'Light Guiding Tetrahedron', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 35 — Dori
-(35, 'Kalpalata Lotus', 'Thunderclap Fruitcore', NOW(), NOW()),
+(35, 'Kalpalata Lotus', 'Thunderclap Fruitcore', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 36 — Faruzan
-(36, 'Henna Berry', 'Light Guiding Tetrahedron', NOW(), NOW()),
+(36, 'Henna Berry', 'Light Guiding Tetrahedron',"Va­yu­da Tur­quoise", NOW(), NOW()),
 
 -- 37 — Kaveh
-(37, 'Mourning Flower', 'Quelled Creeper', NOW(), NOW()),
+(37, 'Mourning Flower', 'Quelled Creeper',"Na­ga­dus Em­er­ald", NOW(), NOW()),
 
 -- 38 — Layla
-(38, 'Nilotpala Lotus', 'Perpetual Caliber', NOW(), NOW()),
+(38, 'Nilotpala Lotus', 'Perpetual Caliber',"Shivada Jade", NOW(), NOW()),
 
 -- 39 — Nahida
-(39, 'Kalpalata Lotus', 'Quelled Creeper', NOW(), NOW()),
+(39, 'Kalpalata Lotus', 'Quelled Creeper',"Na­ga­dus Em­er­ald", NOW(), NOW()),
 
 -- 40 — Nilou
-(40, 'Padisarah', 'Perpetual Caliber', NOW(), NOW()),
+(40, 'Padisarah', 'Perpetual Caliber', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 41 — Sethos
-(41, 'Trishiraite', 'Cloud­seam Scale', NOW(), NOW()),
+(41, 'Trishiraite', 'Cloud­seam Scale', "Vaj­ra­da A­me­thyst", NOW(), NOW()),
 
 -- 42 — Tighnari
-(42, 'Nilotpala Lotus', 'Majestic Hooked Beak', NOW(), NOW()),
+(42, 'Nilotpala Lotus', 'Majestic Hooked Beak',"Na­ga­dus Em­er­ald", NOW(), NOW()),
 
 -- 43 — Wanderer
-(43, 'Rukkhashava Mushroom', 'Perpetual Caliber', NOW(), NOW()),
+(43, 'Rukkhashava Mushroom', 'Perpetual Caliber', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 44 — Baizhu
-(44, 'Violetgrass', 'Evergloom Ring', NOW(), NOW()),
+(44, 'Violetgrass', 'Evergloom Ring',"Na­ga­dus Em­er­ald", NOW(), NOW()),
 
 -- 45 — Beidou
-(45, 'Noctilucous Jade', 'Lightning Prism', NOW(), NOW()),
+(45, 'Noctilucous Jade', 'Lightning Prism', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 46 — Chongyun
-(46, 'Cor Lapis', 'Hoarfrost Core', NOW(), NOW()),
+(46, 'Cor Lapis', 'Hoarfrost Core',"Shivada Jade", NOW(), NOW()),
 
 -- 47 — Gaming
-(47, 'Starconch', 'Emperor’s Resolution', NOW(), NOW()),
+(47, 'Starconch', 'Emperor’s Resolution',"Ag­ni­dus A­gate", NOW(), NOW()),
 
 -- 48 — Ganyu
-(48, 'Qingxin', 'Hoarfrost Core', NOW(), NOW()),
+(48, 'Qingxin', 'Hoarfrost Core',"Shivada Jade", NOW(), NOW()),
 
 -- 49 — Hu Tao
-(49, 'Silk Flower', 'Juvenile Jade', NOW(), NOW()),
+(49, 'Silk Flower', 'Juvenile Jade', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 50 — Keqing
-(50, 'Cor Lapis', 'Lightning Prism', NOW(), NOW()),
+(50, 'Cor Lapis', 'Lightning Prism', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 51 — Lan Yan
-(51, 'Clearwater Jade', 'Gold-Inscribed Secret Source Core', NOW(), NOW()),
+(51, 'Clearwater Jade', 'Gold-Inscribed Secret Source Core', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 52 — Ningguang
-(52, 'Glaze Lily', 'Basalt Pillar', NOW(), NOW()),
+(52, 'Glaze Lily', 'Basalt Pillar', "Prithiva Topaz",NOW(), NOW()),
 
 -- 53 — Qiqi
-(53, 'Violetgrass', 'Hoarfrost Core', NOW(), NOW()),
+(53, 'Violetgrass', 'Hoarfrost Core',"Shivada Jade", NOW(), NOW()),
 
 -- 54 — Shenhe
-(54, 'Qingxin', 'Dragonheir’s False Fin', NOW(), NOW()),
+(54, 'Qingxin', 'Dragonheir’s False Fin',"Shivada Jade", NOW(), NOW()),
 
 -- 55 — Xiangling
-(55, 'Jueyun Chili', 'Everflame Seed', NOW(), NOW()),
+(55, 'Jueyun Chili', 'Everflame Seed', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 56 — Xianyun
-(56, 'Pluie Lotus', 'Cloudseam Scale', NOW(), NOW()),
+(56, 'Pluie Lotus', 'Cloudseam Scale',"Va­yu­da Tur­quoise", NOW(), NOW()),
 
 -- 57 — Xiao
-(57, 'Qingxin', 'Juvenile Jade', NOW(), NOW()),
+(57, 'Qingxin', 'Juvenile Jade',"Va­yu­da Tur­quoise", NOW(), NOW()),
 
 -- 58 — Xingqiu
-(58, 'Silk Flower', 'Cleansing Heart', NOW(), NOW()),
+(58, 'Silk Flower', 'Cleansing Heart', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 59 — Xinyan
-(59, 'Violetgrass', 'Everflame Seed', NOW(), NOW()),
+(59, 'Violetgrass', 'Everflame Seed', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 60 — Yanfei
-(60, 'Noctilucous Jade', 'Juvenile Jade', NOW(), NOW()),
+(60, 'Noctilucous Jade', 'Juvenile Jade', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 61 — Yaoyao
-(61, 'Jueyun Chili', 'Quelled Creeper', NOW(), NOW()),
+(61, 'Jueyun Chili', 'Quelled Creeper',"Na­ga­dus Em­er­ald", NOW(), NOW()),
 
 -- 62 — Yelan
-(62, 'Starconch', 'Runic Fang', NOW(), NOW()),
+(62, 'Starconch', 'Runic Fang', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 63 — Yun Jin
-(63, 'Glaze Lily', 'Riftborn Regalia', NOW(), NOW()),
+(63, 'Glaze Lily', 'Riftborn Regalia',"Prithiva Topaz", NOW(), NOW()),
 
 -- 64 — Zhongli
-(64, 'Cor Lapis', 'Basalt Pillar', NOW(), NOW()),
+(64, 'Cor Lapis', 'Basalt Pillar',"Prithiva Topaz", NOW(), NOW()),
 
 -- 65 — Albedo
-(65, 'Cecilia', 'Basalt Pillar', NOW(), NOW()),
+(65, 'Cecilia', 'Basalt Pillar', "Prithiva Topaz",NOW(), NOW()),
 
 -- 66 — Amber
-(66, 'Small Lamp Grass', 'Everflame Seed', NOW(), NOW()),
+(66, 'Small Lamp Grass', 'Everflame Seed', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 67 — Barbara
-(67, 'Philanemo Mushroom', 'Cleansing Heart', NOW(), NOW()),
+(67, 'Philanemo Mushroom', 'Cleansing Heart', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 68 — Bennett
-(68, 'Windwheel Aster', 'Everflame Seed', NOW(), NOW()),
+(68, 'Windwheel Aster', 'Everflame Seed', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 69 — Diluc
-(69, 'Small Lamp Grass', 'Everflame Seed', NOW(), NOW()),
+(69, 'Small Lamp Grass', 'Everflame Seed',"Ag­ni­dus A­gate", NOW(), NOW()),
 
 -- 70 — Diona
-(70, 'Calla Lily', 'Hoarfrost Core', NOW(), NOW()),
+(70, 'Calla Lily', 'Hoarfrost Core',"Shivada Jade", NOW(), NOW()),
 
 -- 71 — Eula
-(71, 'Dandelion Seed', 'Crystalline Bloom', NOW(), NOW()),
+(71, 'Dandelion Seed', 'Crystalline Bloom',"Shivada Jade", NOW(), NOW()),
 
 -- 72 — Fischl
-(72, 'Small Lamp Grass', 'Lightning Prism', NOW(), NOW()),
+(72, 'Small Lamp Grass', 'Lightning Prism', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 73 — Jean
-(73, 'Dandelion Seed', 'Hurricane Seed', NOW(), NOW()),
+(73, 'Dandelion Seed', 'Hurricane Seed',"Va­yu­da Tur­quoise", NOW(), NOW()),
 
 -- 74 — Kaeya
-(74, 'Calla Lily', 'Hoarfrost Core', NOW(), NOW()),
+(74, 'Calla Lily', 'Hoarfrost Core',"Shivada Jade", NOW(), NOW()),
 
 -- 75 — Klee
-(75, 'Philanemo Mushroom', 'Everflame Seed', NOW(), NOW()),
+(75, 'Philanemo Mushroom', 'Everflame Seed', "Ag­ni­dus A­gate",NOW(), NOW()),
 
 -- 76 — Lisa
-(76, 'Valberry', 'Lightning Prism', NOW(), NOW()),
+(76, 'Valberry', 'Lightning Prism', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 77 — Mika
-(77, 'Wolfhook', 'Pseudostamens', NOW(), NOW()),
+(77, 'Wolfhook', 'Pseudostamens',"Shivada Jade", NOW(), NOW()),
 
 -- 78 — Mona
-(78, 'Philanemo Mushroom', 'Cleansing Heart', NOW(), NOW()),
+(78, 'Philanemo Mushroom', 'Cleansing Heart', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 79 — Noelle
-(79, 'Valberry', 'Basalt Pillar', NOW(), NOW()),
+(79, 'Valberry', 'Basalt Pillar',"Prithiva Topaz", NOW(), NOW()),
 
 -- 80 — Razor
-(80, 'Wolfhook', 'Lightning Prism', NOW(), NOW()),
+(80, 'Wolfhook', 'Lightning Prism', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 81 — Rosaria
-(81, 'Valberry', 'Hoarfrost Core', NOW(), NOW()),
+(81, 'Valberry', 'Hoarfrost Core',"Shivada Jade", NOW(), NOW()),
 
 -- 82 — Sucrose
-(82, 'Windwheel Aster', 'Hurricane Seed', NOW(), NOW()),
+(82, 'Windwheel Aster', 'Hurricane Seed', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 83 — Venti
-(83, 'Cecilia', 'Hurricane Seed', NOW(), NOW()),
+(83, 'Cecilia', 'Hurricane Seed', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 84 — Arlecchino
-(84, 'Rainbow Rose', 'Fragment of a Golden Melody', NOW(), NOW()),
+(84, 'Rainbow Rose', 'Fragment of a Golden Melody',"Ag­ni­dus A­gate", NOW(), NOW()),
 
 -- 85 — Tartaglia (Childe)
-(85, 'Starconch', 'Cleansing Heart', NOW(), NOW()),
+(85, 'Starconch', 'Cleansing Heart',"Va­ru­na­da La­zu­rite ", NOW(), NOW()),
 
 -- 86 — Chasca
-(86, 'Withering Purpurbloom', 'Ensnaring Gaze', NOW(), NOW()),
+(86, 'Withering Purpurbloom', 'Ensnaring Gaze', "Va­yu­da Tur­quoise",NOW(), NOW()),
 
 -- 87 — Citlali
-(87, 'Quenepa Berry', 'Talisman of the Enigmatic Land', NOW(), NOW()),
+(87, 'Quenepa Berry', 'Talisman of the Enigmatic Land',"Shivada Jade", NOW(), NOW()),
 
 -- 88 — Iansan
-(88, 'Dracolite', 'Ensnaring Gaze', NOW(), NOW()),
+(88, 'Dracolite', 'Ensnaring Gaze', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 89 — Kachina
-(89, 'Quenepa Berry', 'Over­ripe Flame­gra­nate', NOW(), NOW()),
+(89, 'Quenepa Berry', 'Over­ripe Flame­gra­nate', "Prithiva Topaz",NOW(), NOW()),
 
 -- 90 — Kinich
-(90, 'Saurian Claw Succulent', 'Over­ripe Flame­gra­nate', NOW(), NOW()),
+(90, 'Saurian Claw Succulent', 'Over­ripe Flame­gra­nate', "Na­ga­dus Em­er­ald",NOW(), NOW()),
 
 -- 91 — Mavuika
-(91, 'Withering Purpurbloom', 'Gold-Inscribed Secret Source Core', NOW(), NOW()),
+(91, 'Withering Purpurbloom', 'Gold-Inscribed Secret Source Core',"Ag­ni­dus A­gate", NOW(), NOW()),
 
 -- 92 — Mualani
-(92, 'Sprayfeather Gill', 'Mark of the Binding Blessing', NOW(), NOW()),
+(92, 'Sprayfeather Gill', 'Mark of the Binding Blessing', "Va­ru­na­da La­zu­rite ",NOW(), NOW()),
 
 -- 93 — Ororon
-(93, 'Glowing Hornshroom', 'Mark of the Binding Blessing', NOW(), NOW()),
+(93, 'Glowing Hornshroom', 'Mark of the Binding Blessing', "Vaj­ra­da A­me­thyst",NOW(), NOW()),
 
 -- 94 — Varesa
-(94, 'Skysplit Gembloom', 'Sparkless Statue Core', NOW(), NOW()),
+(94, 'Skysplit Gembloom', 'Sparkless Statue Core', "Vaj­ra­da A­me­thyst", NOW(), NOW()),
 
 -- 95 — Xilonen
-(95, 'Brilliant Chrysanthemum', 'Gold-Inscribed Secret Source Core', NOW(), NOW()),
+(95, 'Brilliant Chrysanthemum', 'Gold-Inscribed Secret Source Core', "Prithiva Topaz",NOW(), NOW()),
 
 -- 96 - Aloy
-(96, 'Crystal Marrow', 'Crys­tal­line Bloom', NOW(), NOW());
+(96, 'Crystal Marrow', 'Crys­tal­line Bloom',"Shivada Jade", NOW(), NOW());
