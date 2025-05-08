b16391b396a317a3a514d48a788930622597be48
Snayt1k3 <snayt1k3twitch@gmail.com>
2025-05-08 21:33:26 +0300

    add buttons

--- bot/internal/handlers/gachas/wuwa/button.go
+++ bot/internal/handlers/gachas/wuwa/button.go
@@ -1,7 +1,75 @@
 package wuwa
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+	"fmt"
+)
 
 func WuwaButtons(id int) []discordgo.MessageComponent {
-	return nil
+	return []discordgo.MessageComponent{
+		discordgo.ActionsRow{
+			Components: []discordgo.MessageComponent{
+				discordgo.Button{
+					Label:    "Back",
+					CustomID: "WuwaCharacters",
+					Style:    discordgo.SecondaryButton,
+					Emoji: &discordgo.ComponentEmoji{
+						Name: "↩️",
+					},
+				},
+				discordgo.Button{
+					Label:    "Ascension",
+					CustomID: fmt.Sprintf("WuwaAscension_%v", id),
+					Style:    discordgo.SecondaryButton,
+					Emoji: &discordgo.ComponentEmoji{
+						Name: "⬆️",
+					},
+				},
+				discordgo.Button{
+					Label:    "Talents",
+					CustomID: fmt.Sprintf("WuwaTalents_%v", id),
+					Style:    discordgo.SecondaryButton,
+					Emoji: &discordgo.ComponentEmoji{
+						Name: "📘",
+					},
+				},
+				discordgo.Button{
+					Label:    "Echoes",
+					CustomID: fmt.Sprintf("WuwaEchoes_%v", id),
+					Style:    discordgo.SecondaryButton,
+					Emoji: &discordgo.ComponentEmoji{
+						Name: "🌀",
+					},
+				},
+			},
+		},
+		discordgo.ActionsRow{
+			Components: []discordgo.MessageComponent{
+				discordgo.Button{
+					Label:    "Weapons",
+					CustomID: fmt.Sprintf("WuwaWeapons_%v", id),
+					Style:    discordgo.SecondaryButton,
+					Emoji: &discordgo.ComponentEmoji{
+						Name: "⚔️",
+					},
+				},
+				discordgo.Button{
+					Label:    "Team Synergy",
+					CustomID: fmt.Sprintf("WuwaTeams_%v", id),
+					Style:    discordgo.SecondaryButton,
+					Emoji: &discordgo.ComponentEmoji{
+						Name: "👥",
+					},
+				},
+				discordgo.Button{
+					Label:    "Overview",
+					CustomID: fmt.Sprintf("WuwaOverview_%v", id),
+					Style:    discordgo.SecondaryButton,
+					Emoji: &discordgo.ComponentEmoji{
+						Name: "📖",
+					},
+				},
+			},
+		},
+	}
 }
